c47920beefb2b4f0a9bd3b3ced2d7fe478d57b94
AvhaleSwapnil <swapnilavhale460@gmail.com>
2024-04-28 01:18:38 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice.js";
 export default function SignIn() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [formData, setformData] = useState({});
-  const [error, seterror] = useState(false);
-  const [loading, setloading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const handleChange = (e) => {
     setformData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -12,27 +18,24 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setloading(true);
-      seterror(false);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      console.log(data);
-      setloading(false);
       if (data.success == false) {
-        seterror(true);
+        dispatch(signInFailure(data));
         return;
       }
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setloading(false);
-      seterror(true);
+      dispatch(signInFailure(error));
     }
   };
-  // sign in code
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
@@ -61,7 +64,9 @@ export default function SignIn() {
           <span className="text-blue-500">Sign Up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && "Wrong Credential...."}</p>
+      <p className="text-red-700 mt-5">
+        {error ? error.message || "Wrong Credential...." : ""}
+      </p>
     </div>
   );
 }
