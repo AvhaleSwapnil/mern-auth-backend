d7029111248316625777141b4b24f99e17414b3d
AvhaleSwapnil <swapnilavhale460@gmail.com>
2025-02-26 08:14:02 +0530

    update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,9 @@ import express from "express";
 import mongoose from "mongoose";
 import authRoutes from "./routes/authRoute.js";
 import userRoutes from "./routes/userRoute.js";
+
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -13,16 +15,15 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("server is listening to port 3000");
-});
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+// Global Error Handling Middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -32,3 +33,5 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+export default app;
